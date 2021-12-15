310f9f996a41bc0aa367495e0c3221efa74a6244
finn-01 <nguyensam576@gmail.com>
2021-12-15 22:01:12 +0700

    to do list two way bindling

--- src/App.js
+++ src/App.js
@@ -1,86 +1,44 @@
 import { useState } from "react";
 
-//const gifts = ["CPU i9", "RAM 32GB RGB", "RGB Keyboard"];
-
-//two way bindling
-//Respone from API
-const courses = [
-	{
-		id: 1,
-		name: "HTML, CSS",
-	},
-	{
-		id: 2,
-		name: "Java",
-	},
-	{
-		id: 3,
-		name: "Python",
-	},
-];
+//to do list
 
 function App() {
-	//gitf
-	//const [gift, setGift] = useState(); //Hien thi chua co phan thuong nen mac dinh la khong co tham so
+	const [job, setJob] = useState("");
+	const [jobs, setJobs] = useState(() => {
+		const storageJobs = JSON.parse(localStorage.getItem("jobs"));
 
-	//console.log(gift + setGift);
-	// const radomGift = () => {
-	// 	const index = Math.floor(Math.random() * gifts.length);
+		console.log(storageJobs);
+		return storageJobs;
+	});
 
-	// 	setGift(gifts[index]); //Thay doi gia tri khi co
-	// 	//console.log(gifts[index]);
-	// };
-
-	//form email, name
-	// const [name, setName] = useState("");
-	// const [email, setEmail] = useState("");
-
-	//courses
-	const [checked, setChecked] = useState([]);
-	console.log(checked);
-
-	const handleCheck = (id) => {
-		setChecked((prev) => {
-			const isChecked = checked.includes(id);
-			if (isChecked) {
-				//uncheck
-				return checked.filter((item) => item !== id); //bo check2 lan
-			} else {
-				return [...prev, id];
-			}
-		});
-	};
+	//console.log(job);
 
 	const handleSubmit = () => {
-		//Call API
-		//form email, name
-		// console.log({
-		// 	name,
-		// 	email,
-		// });
-		//courses
-		console.log({ ids: checked });
-	};
+		setJobs((prev) => {
+			const newJobs = [...prev, job];
 
-	//console.log(name);
+			//Save LocalStorage
+			const jsonJobs = JSON.stringify(newJobs);
+			//console.log(jsonJobs);
+			localStorage.setItem("jobs", jsonJobs);
+
+			return [...prev, job];
+		});
+		setJob("");
+
+		//console.log(jobs);
+	};
 
 	return (
 		<div style={{ padding: 32 }}>
-			{/* <input value={name} onChange={(e) => setName(e.target.value)} />
-			<input value={email} onChange={(e) => setEmail(e.target.value)} />
-			<button onClick={handleSubmit}>Submit</button> */}
-			{courses.map((course) => (
-				<div key={course.id}>
-					<input
-						type="checkbox"
-						checked={checked.includes(course.id)}
-						onChange={() => handleCheck(course.id)} // courses
-					/>
-					{course.name}
-				</div>
-			))}
+			<input value={job} onChange={(e) => setJob(e.target.value)} />
+			<button onClick={handleSubmit}>ADD</button>
 
-			<button onClick={handleSubmit}>Submit</button>
+			<ul>
+				{jobs.map((job, index) => (
+					<li key={index}>{job}</li>
+				))}
+			</ul>
 		</div>
 	);
 }
