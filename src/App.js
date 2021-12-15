a8b61459c679f80b348233efe9a77638f95f9451
finn-01 <nguyensam576@gmail.com>
2021-12-15 14:46:17 +0700

    gift random

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { useState } from "react";
 
-function App() {
-	const [counter, setCounter] = useState(1);
+const gifts = ["CPU i9", "RAM 32GB RGB", "RGB Keyboard"];
 
-	const handleIncrease = () => {
-		setCounter(counter + 1);
+function App() {
+	const [gift, setGift] = useState(); //Hien thi chua co phan thuong nen mac dinh la khong co tham so
+
+	//console.log(gift + setGift);
+	const radomGift = () => {
+		const index = Math.floor(Math.random() * gifts.length);
+
+		setGift(gifts[index]); //Thay doi gia tri khi co
+		//console.log(gifts[index]);
 	};
 
 	return (
-		<div className="App">
-			<h1>{counter}</h1>
-			<button onClick={handleIncrease}>Increase</button>
+		<div style={{ padding: 32 }}>
+			<h1>{gift || "Chưa có phần thưởng"}</h1>
+			<button onClick={radomGift}>Lay thuong</button>
 		</div>
 	);
 }
