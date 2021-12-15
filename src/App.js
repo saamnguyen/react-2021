eb17df97d48cfbc98b65614559f08ac09609073f
finn-01 <nguyensam576@gmail.com>
2021-12-15 15:08:01 +0700

    one way binding

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import { useState } from "react";
 
-const gifts = ["CPU i9", "RAM 32GB RGB", "RGB Keyboard"];
+//const gifts = ["CPU i9", "RAM 32GB RGB", "RGB Keyboard"];
 
 function App() {
-	const [gift, setGift] = useState(); //Hien thi chua co phan thuong nen mac dinh la khong co tham so
+	//const [gift, setGift] = useState(); //Hien thi chua co phan thuong nen mac dinh la khong co tham so
 
 	//console.log(gift + setGift);
-	const radomGift = () => {
-		const index = Math.floor(Math.random() * gifts.length);
+	// const radomGift = () => {
+	// 	const index = Math.floor(Math.random() * gifts.length);
 
-		setGift(gifts[index]); //Thay doi gia tri khi co
-		//console.log(gifts[index]);
-	};
+	// 	setGift(gifts[index]); //Thay doi gia tri khi co
+	// 	//console.log(gifts[index]);
+	// };
+
+	const [name, setName] = useState("");
+
+	console.log(name);
 
 	return (
 		<div style={{ padding: 32 }}>
-			<h1>{gift || "Chưa có phần thưởng"}</h1>
-			<button onClick={radomGift}>Lay thuong</button>
+			<input onChange={(e) => setName(e.target.value)} />
 		</div>
 	);
 }
