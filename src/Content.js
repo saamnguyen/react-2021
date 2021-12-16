5f79db51f7621320a045ae8650c97293eb6c7288
finn-01 <nguyensam576@gmail.com>
2021-12-17 01:27:22 +0700

    useEffect() with DOM events - resize

--- src/Content.js
+++ src/Content.js
@@ -1,46 +1,23 @@
 import { useEffect, useState } from "react";
 
-const tabs = ["posts", "comments", "albums"];
-
 function Content() {
-	const [title, setTitle] = useState("");
-	const [posts, setPosts] = useState([]);
-	const [type, setType] = useState("posts");
-	const [showGoToTop, setShowGoToTop] = useState(false);
-
-	//console.log(type);
+	const [width, setWidth] = useState(window.innerWidth);
 
 	useEffect(() => {
-		fetch(`https://jsonplaceholder.typicode.com/${type}`)
-			.then((res) => res.json())
-			.then((posts) => {
-				setPosts(posts);
-			});
-	}, [type]);
+		const handleResize = () => {
+			setWidth(window.innerWidth);
+		};
+
+		window.addEventListener("resize", handleResize);
+
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
 
 	return (
 		<div>
-			{tabs.map((tab) => (
-				<button
-					key={tab}
-					onClick={() => setType(tab)}
-					style={type === tab ? { color: "#fff", backgroundColor: "#333" } : {}}
-				>
-					{tab}
-				</button>
-			))}
-
-			<input value={title} onChange={(e) => setTitle(e.target.value)} />
-			<ul>
-				{posts.map((post) => (
-					<li key={post.id}>{post.title || post.name}</li>
-				))}
-			</ul>
-			{showGoToTop && (
-				<button style={{ position: "fixed", right: 20, bottom: 20 }}>
-					Go to top
-				</button>
-			)}
+			<h1>{width}</h1>
 		</div>
 	);
 }
