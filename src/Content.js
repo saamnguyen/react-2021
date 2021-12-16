9b67d99c3e6af6a6e661093cb76c78c6a3b60251
finn-01 <nguyensam576@gmail.com>
2021-12-16 15:51:34 +0700

    change title and th2 useEffect new man

--- src/Content.js
+++ src/Content.js
@@ -1,7 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Content() {
-	return <h1>Hello world!</h1>;
+	const [title, setTitle] = useState("");
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		fetch("https://jsonplaceholder.typicode.com/posts")
+			.then((res) => res.json())
+			.then((posts) => {
+				setPosts(posts);
+			});
+	}, []);
+
+	return (
+		<div>
+			<input value={title} onChange={(e) => setTitle(e.target.value)} />
+			<ul>
+				{posts.map((post) => (
+					<li key={post.id}>{post.title}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default Content;
