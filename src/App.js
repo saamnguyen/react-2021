8d44157ce29ab419bb1b1ea3bb515506147af2b7
finn-01 <nguyensam576@gmail.com>
2021-12-16 14:35:34 +0700

    mounted and unmounted

--- src/App.js
+++ src/App.js
@@ -1,44 +1,15 @@
 import { useState } from "react";
+import Content from "./Content";
 
 //to do list
 
 function App() {
-	const [job, setJob] = useState("");
-	const [jobs, setJobs] = useState(() => {
-		const storageJobs = JSON.parse(localStorage.getItem("jobs"));
-
-		console.log(storageJobs);
-		return storageJobs;
-	});
-
-	//console.log(job);
-
-	const handleSubmit = () => {
-		setJobs((prev) => {
-			const newJobs = [...prev, job];
-
-			//Save LocalStorage
-			const jsonJobs = JSON.stringify(newJobs);
-			//console.log(jsonJobs);
-			localStorage.setItem("jobs", jsonJobs);
-
-			return [...prev, job];
-		});
-		setJob("");
-
-		//console.log(jobs);
-	};
+	const [show, setShow] = useState(false);
 
 	return (
 		<div style={{ padding: 32 }}>
-			<input value={job} onChange={(e) => setJob(e.target.value)} />
-			<button onClick={handleSubmit}>ADD</button>
-
-			<ul>
-				{jobs.map((job, index) => (
-					<li key={index}>{job}</li>
-				))}
-			</ul>
+			<button onClick={() => setShow(!show)}>Toggle</button>
+			{show && <Content />}
 		</div>
 	);
 }
---
+++ src/Content.js
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+function Content() {
+	return <h1>Hello world!</h1>;
+}
+
+export default Content;
