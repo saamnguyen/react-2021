b0b8b02900fdef186c65a279645d06d05906e8a2
finn-01 <nguyensam576@gmail.com>
2021-12-18 14:49:30 +0700

    useRef() hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import Content from "./Content";
 
 //to do list
 
 function App() {
-	const [show, setShow] = useState(false);
+	const [count, setCount] = useState(60);
+
+	const timerId = useRef();
+	const prevCount = useRef();
+
+	const handleStart = () => {
+		timerId.current = setInterval(() => {
+			setCount((prev) => prev - 1);
+		}, 1000);
+		console.log(timerId + "start");
+	};
+
+	const handleStop = () => {
+		clearInterval(timerId.current);
+		console.log(timerId + "stop");
+	};
+
+	useEffect(() => {
+		prevCount.current = count;
+	}, [count]);
+
+	console.log(count, prevCount.current);
 
 	return (
 		<div style={{ padding: 32 }}>
-			<button onClick={() => setShow(!show)}>Toggle</button>
-			{show && <Content />}
+			<h1>{count}</h1>
+			<button onClick={handleStart}>Start</button>
+			<button onClick={handleStop}>Stop</button>
 		</div>
 	);
 }
