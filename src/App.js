0b0877f8d7de9b1c9a48a40ab9acd5ab0a0eec97
finn-01 <nguyensam576@gmail.com>
2021-12-19 15:06:54 +0700

    useReducer() hook

--- src/App.js
+++ src/App.js
@@ -1,64 +1,35 @@
-import { useState, useMemo, useRef } from "react";
+import { useState, useReducer } from "react";
 import Content from "./Content";
 
 //to do list
+//Init State
+const initState = 0;
+
+//Actions
+const UP_ACTION = "up";
+const DOWN_ACTION = "down";
+
+//Reducer
+const reducer = (state, action) => {
+	console.log("reducer running...");
+	switch (action) {
+		case UP_ACTION:
+			return state + 1;
+		case DOWN_ACTION:
+			return state - 1;
+		default:
+			throw new Error("Invalid actions");
+	}
+};
 
 function App() {
-	const [name, setName] = useState("");
-	const [price, setPrice] = useState("");
-	const [products, setProducts] = useState([]);
-
-	const nameRef = useRef();
-
-	const handleSubmit = () => {
-		setProducts([
-			...products,
-			{
-				name,
-				price: parseInt(price),
-			},
-		]);
-		setName("");
-		setPrice("");
-		nameRef.current.focus();
-	};
-
-	//console.log(typeof price);
-	//console.log(products);
-	const total = useMemo(() => {
-		const result = products.reduce((result, prod) => {
-			console.log("Tinh lai");
-			return result + prod.price;
-		}, 0);
-
-		return result;
-	}, [products]);
+	const [count, dispatch] = useReducer(reducer, initState);
 
 	return (
 		<div style={{ padding: 32 }}>
-			<input
-				value={name}
-				ref={nameRef}
-				placeholder="Enter name..."
-				onChange={(e) => setName(e.target.value)}
-			/>
-			<br />
-			<input
-				value={price}
-				placeholder="Enter price..."
-				onChange={(e) => setPrice(e.target.value)}
-			/>
-			<br />
-			<button onClick={handleSubmit}>Submit</button>
-			<br />
-			Total: {total}
-			<ul>
-				{products.map((product, index) => (
-					<li key={index}>
-						{product.name} - {product.price}
-					</li>
-				))}
-			</ul>
+			<h1>{count}</h1>
+			<button onClick={() => dispatch(DOWN_ACTION)}>Down</button>
+			<button onClick={() => dispatch(UP_ACTION)}>Up</button>
 		</div>
 	);
 }
